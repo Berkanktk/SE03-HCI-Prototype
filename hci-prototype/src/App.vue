396234d4e7905e9df00a5d69f1e2ffa7142d9d17
Berkan Kütük <bekut20@student.sdu.dk>
2021-11-30 11:25:56 +0100

    Den fucker med mit form så den er gone:D

--- hci-prototype/src/App.vue
+++ hci-prototype/src/App.vue
@@ -14,7 +14,6 @@
   font-family: Roboto, Helvetica, Arial, sans-serif, Inter;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
