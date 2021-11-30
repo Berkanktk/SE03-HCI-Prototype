9dd80a738888ecd6265b4f31fd144b91818ec497
Ashvikan Sivabalaa <ashvikan10@gmail.com>
2021-11-30 10:11:46 +0100

    Nav update

--- hci-prototype/src/components/NavigationBar.vue
+++ hci-prototype/src/components/NavigationBar.vue
@@ -1,39 +1,21 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <b-navbar class="navbar" >
+      <b-navbar-brand href="#">Ticketo</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item v-bind:to="'/'">Home</b-nav-item>
-          <b-nav-item v-bind:to="'/about'">About</b-nav-item>
-          <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          <b-nav-item href="#">Opret ticket</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ms-auto">
           <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Søg"></b-form-input>
+            <b-button size="lg" class="my-2 my-sm-0" type="submit">Søg</b-button>
           </b-nav-form>
-
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown right>
-            <!-- Using 'button-content' slot -->
-            <template #button-content>
-              <em>User</em>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -51,11 +33,18 @@ export default {
 .form-inline{
   display: flex;
 }
+
 .mr-sm-2{
   margin-right: 8px;
 }
+.my-sm-0{
+  background-color: #7749F8;
+  border-color: #ffffff;
+}
 .navbar{
   padding:12px;
-}
+  background-color: #7749F8;
+  border-radius: 4px;
 
+}
 </style>
