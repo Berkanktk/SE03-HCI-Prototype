3adf4b97d6ed56e356f227842dea84c123c5fffd
Esben Sørensen <esben@esben.io>
2021-11-30 09:23:31 +0100

    Added Vue router

--- hci-prototype/src/main.js
+++ hci-prototype/src/main.js
@@ -4,6 +4,7 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import router from './router'
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,6 @@ Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
