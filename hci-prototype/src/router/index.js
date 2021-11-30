564a99521c39d792364ac38cba471de48fb205fe
Esben Sørensen <esben@esben.io>
2021-11-30 11:40:05 +0100

    Added / alias for search page

--- hci-prototype/src/router/index.js
+++ hci-prototype/src/router/index.js
@@ -1,16 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import create from '../views/Create.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
   {
     path: '/about',
     name: 'About',
@@ -22,6 +16,7 @@ const routes = [
   {
     path: '/search',
     name: 'Search',
+    alias: '/',
     component: () => import('../views/SearchPage.vue')
   },
   {
